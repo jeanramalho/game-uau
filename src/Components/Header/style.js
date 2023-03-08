0c6b28c191d1d46ec8a876ec7b582a253b4a3417
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-08 16:19:53 -0300

    Começa a estilizar Header

--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -1,5 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    
+    container:{
+        width: 100,
+    },
+    header: {
+        backgroundColor: "#00ffff",
+        width: "100%"
+    }
 })
