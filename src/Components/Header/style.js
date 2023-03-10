a8d4db261bc6c9a9b94fdf7d97f6ae0a02044ede
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-09 21:40:47 -0300

    Ajustando header

--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -4,6 +4,8 @@ const styles = StyleSheet.create({
     container:{
         width: 100,
         padding: 0,
+        flex: 1,
+        marginTop: 0,
     },
     header: {
         backgroundColor: "#00ffff",
