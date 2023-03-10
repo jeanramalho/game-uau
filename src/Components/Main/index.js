6901d250465c2a01161a32fb1b1c215c4ea0d971
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-09 21:42:16 -0300

    Criando componente Main

---
+++ src/Components/Main/index.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import styles from "./style"
+
+export class index extends Component {
+  render() {
+    return (
+      <View style={styles.header}>
+        <Text>Game UAU</Text>
+      </View>
+    )
+  }
+}
+
+export default index
---
+++ src/Components/Main/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        width: 100,
+        padding: 0,
+        flex: 1,
+        marginTop: 0,
+    },
+    header: {
+        backgroundColor: "#00ffff",
+        width: "100%",
+        height: 30,
+        marginTop: 0,
+        paddingTop: 0,
+    }
+})
+
+export default styles
