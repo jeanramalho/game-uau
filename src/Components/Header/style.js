93ebcec599fb6ded3d3c6b3e0dd43c970dc32780
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-09 21:37:04 -0300

    Ainda tentando resolver topo no header

--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -3,12 +3,14 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container:{
         width: 100,
+        padding: 0,
     },
     header: {
         backgroundColor: "#00ffff",
         width: "100%",
         height: 30,
         marginTop: 0,
+        paddingTop: 0,
     }
 })
 
