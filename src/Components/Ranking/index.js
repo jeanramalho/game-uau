4bd3fc351870b0d503241165e6e62aaca8042a50
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-27 10:04:58 -0300

    Criando estrutura de view

--- src/Components/Ranking/index.js
+++ src/Components/Ranking/index.js
@@ -1,16 +1,12 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import React, { Component } from 'react'
-import Header from '../Header/'
-import Input from "../Input/"
-
 
 
 export class index extends Component {
   render() {
     return (
       <View>
-        <Header/>
-        <Input />
+        
       </View>
     )
   }
