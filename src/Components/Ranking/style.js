6c40e59d226cbb32042b2bfb208e40b7785f2551
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-27 10:08:06 -0300

    Cria div para colocar Ranking

--- src/Components/Ranking/style.js
+++ src/Components/Ranking/style.js
@@ -2,20 +2,13 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        width: "100%",
-        padding: 0,
+        width: "90%",
+        padding: 10,
         flex: 1,
         marginTop: 0,
+        backgroundColor: "#cecece",
     },
-    input:{
-        marginTop: 50,
-        width:"90%",
-        borderRadius: 50,
-        backgroundColor: '#f6f6f6',
-        height: 40,
-        margin: 12,
-        paddingLeft: 10,
-    }
+   
 })
 
 export default styles
