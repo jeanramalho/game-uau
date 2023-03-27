c04dd9a178702e6d15d45bbaca7f6eccb3ffdf4c
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-27 10:10:18 -0300

    Ajusta main

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,14 +2,15 @@ import { Text, View } from 'react-native'
 import React, { Component } from 'react'
 import Header from '../Header/'
 import Input from '../Input/'
+import Ranking from '../Ranking/'
 
 
 export class index extends Component {
   render() {
     return (
       <View>
-        <Header/>
-        <Input/>
+        <Header />
+        <Ranking />
       </View>
     )
   }
