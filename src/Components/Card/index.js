21cc7c115e71d4bde97a3231afbb9f97b773903e
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-28 09:12:55 -0300

    Cria componente de card

---
+++ src/Components/Card/index.js
@@ -0,0 +1,16 @@
+import {Text, View } from 'react-native'
+import React, { Component } from 'react'
+import styles from "./style"
+
+
+export class index extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    )
+  }
+}
+
+export default index
