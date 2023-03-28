22957b3da9c102fd36e0631141b8e661860c05fc
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-28 09:46:52 -0300

    Ajustando espaçamento dentro do card

--- src/Components/Card/style.js
+++ src/Components/Card/style.js
@@ -7,11 +7,13 @@ const styles = StyleSheet.create({
         marginTop: 0,
         backgroundColor: "#f1f1f1",
         borderRadius: 8,
+        flexDirection: "row",
+        alignContent: "center",
+        justifyContent: "space-around",
     },
 
     participante: {
         fontSize: 15,
-
     }
 
     
