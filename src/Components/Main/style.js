bbb8522c3ed30aded581952e3532fe1aff56857c
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-29 08:56:17 -0300

    muda alinhamento de rankings

--- src/Components/Main/style.js
+++ src/Components/Main/style.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "80%",
         alignItems: "center",
-        justifyContent: "space-between",
+        justifyContent: "space-evenly",
     }
 
  
