f84374da4010de4d333313396e72221946a214c6
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-31 09:38:17 -0300

    Não consegue fazer tela aparecer na navegação

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
-import  Routes  from '../../Routes'
+import  {Routes}  from '../../Routes'
 import styles from './style'
 
 
