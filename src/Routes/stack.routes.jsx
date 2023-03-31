c88819124d7ae8ff23c80512c3c283cef69c4a0a
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-31 09:08:46 -0300

    Importa screen home

--- src/Routes/stack.routes.jsx
+++ src/Routes/stack.routes.jsx
@@ -1,5 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
+import Home from "../Screens/Home"
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
+export function StackRoutes(){
+    return (
+        <Navigator>
+            <Screen
+              name="Home"  
+              component={Home}
+            />
+        </Navigator>
+    )
+}
