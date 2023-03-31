ac4d2b7cea1ee255f09abc615a0f695f8330decf
jeanramalho <jeanramalho.dev@gmail.com>
2023-03-31 09:16:13 -0300

    Ajusta ranking de expotação

--- src/Routes/stack.routes.jsx
+++ src/Routes/stack.routes.jsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from "../Screens/Home/Home"
+import CompleteRanking from '../Screens/CompleteRanking/CompleteRanking';
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
@@ -9,6 +10,11 @@ export function StackRoutes(){
             <Screen
               name="Home"  
               component={Home}
+            />
+
+        <Screen
+              name="CompleteRanking"  
+              component={CompleteRanking}
             />
         </Navigator>
     )
