456af247e6758109336eca6c55f443202142cc23
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-04 08:47:17 -0300

    Implementa compoonente scrollview

--- src/Screens/CompleteRanking/CompleteRanking.jsx
+++ src/Screens/CompleteRanking/CompleteRanking.jsx
@@ -11,30 +11,30 @@ const CompleteRanking = () => {
  
     return (
       <View style={styles.container}>
-        
-        <Header />       
-        <Text style={styles.title}>Ranking Liderança</Text>
-        <View style={styles.rankingContainer}>               
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-          <Card pontos={100} participante={"Jean Ramalho"}/>
-          <Card pontos={90} participante={"Victor Silva"}/>
-          <Card pontos={80} participante={"Carol Deó"}/>
-        </View>
-
+        <ScrollView>
+          <Header />       
+          <Text style={styles.title}>Ranking Liderança</Text>
+          <View style={styles.rankingContainer}>               
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+            <Card pontos={100} participante={"Jean Ramalho"}/>
+            <Card pontos={90} participante={"Victor Silva"}/>
+            <Card pontos={80} participante={"Carol Deó"}/>
+          </View>
+        </ScrollView>
       </View>
     )
   
