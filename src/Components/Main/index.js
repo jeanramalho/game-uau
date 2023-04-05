f0867c17304489ed2419a7ead42c22458bb303eb
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-05 09:40:53 -0300

    Tentando exibir na tela

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -3,24 +3,30 @@ import React, { Component } from 'react'
 import Header from '../Header'
 import Ranking from '../Ranking'
 import styles from './style'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../../Lib/apollo'
 
 
 export const Main = () => {
 
     return (
-      <View style={styles.container}>
-           
-              <View style={styles.container}>
-                <Header />
-                
-                <View style={styles.rankings}>  
-                  <Ranking tipo="lid" />
-                  <Ranking tipo="classe" />
-                </View>
-                
+      <ApolloProvider client={client}>
+          <View style={styles.container}>
+            
+            <View style={styles.container}>
+              <Header />
+              
+              <View style={styles.rankings}>  
+                <Ranking tipo="lid" />
+                <Ranking tipo="classe" />
               </View>
-         
-      </View>
+              
+            </View>
+      
+           </View>
+
+      </ApolloProvider>
+      
     )
   }
 
