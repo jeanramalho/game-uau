0668a6e4d88f3f018d1070e45d053f810fee7e57
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-05 09:10:16 -0300

    Cria configurações de apollo client

---
+++ src/Lib/apollo.jsx
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://api-sa-east-1.hygraph.com/v2/clg2990l0090i01t3gyurhaq1/master/upload',
+    cache: new InMemoryCache()
+
+})
