8f5f4be3f906aed75e3f9b6ee9f135eb354c2bae
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-06 09:51:21 -0300

    Consegue ajustar nome e icone

--- src/Routes/drawer.routes.jsx
+++ src/Routes/drawer.routes.jsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import  {Home}  from '../Screens/Home/Home'
 import CompleteRanking from '../Screens/CompleteRanking/CompleteRanking';
 import CompleteRankingClasse from '../Screens/CompleteRankingClasse/CompleteRankingClasse'
+import { MaterialIcons } from '@expo/vector-icons'
 
 
 const { Screen, Navigator } = createDrawerNavigator();
@@ -12,6 +13,10 @@ export function DrawerRoutes(){
             <Screen
               name='Home'  
               component={Home}
+              options={{
+                drawerLabel: "Início",
+                drawerIcon: () => <MaterialIcons  name="home" size={22} />
+              }}
             />
 
             <Screen
