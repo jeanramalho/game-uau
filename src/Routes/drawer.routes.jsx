6f898b7f9780b306200965b4efb8d322246abd43
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-06 08:37:40 -0300

    Cria arquivo de drawer routes

---
+++ src/Routes/drawer.routes.jsx
@@ -0,0 +1,37 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import  {Home}  from '../Screens/Home/Home'
+import CompleteRanking from '../Screens/CompleteRanking/CompleteRanking';
+import CompleteRankingClasse from '../Screens/CompleteRankingClasse/CompleteRankingClasse'
+
+
+const { Screen, Navigator } = createNativeStackNavigator();
+
+export function StackRoutes(){
+    return (
+        <Navigator>
+            <Screen
+              name='Home'  
+              options={{
+                headerShown: false,
+              }}
+              component={Home}
+            />
+
+            <Screen
+              name='CompleteRanking'  
+              options={{
+                headerShown: false,
+              }}
+              component={CompleteRanking}
+            />
+
+            <Screen
+              name='CompleteRankingClasse'  
+              options={{
+                headerShown: false,
+              }}
+              component={CompleteRankingClasse}
+            />
+        </Navigator>
+    )
+}
