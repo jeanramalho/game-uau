243f49469364f549b9ee9dd0e7e798b7fa2c0227
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-06 08:44:48 -0300

    Cria e ajusta o drawer routes no index

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StackRoutes } from "./stack.routes"
+import { DrawerRoutes } from "./drawer.routes"
 
 export function Routes(){
     return (
     <NavigationContainer>
+        <DrawerRoutes />
         <StackRoutes />
     </NavigationContainer>
     )
