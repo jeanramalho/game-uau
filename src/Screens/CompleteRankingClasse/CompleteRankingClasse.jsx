c48f57bfa3e1af4ee5124916a9da43cc3780e5ae
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-07 11:05:19 -0300

    Retira header de mais uma página

--- src/Screens/CompleteRankingClasse/CompleteRankingClasse.jsx
+++ src/Screens/CompleteRankingClasse/CompleteRankingClasse.jsx
@@ -1,6 +1,5 @@
 import { Text, View, ScrollView } from 'react-native'
 import React, { Component } from 'react'
-import Header from '../../Components/Header'
 import styles from './style'
 import Card from '../../Components/Card'
 import Ranking from '../../Components/Ranking'
@@ -17,7 +16,6 @@ const CompleteRanking = () => {
     <ApolloProvider client={client}>
 
       <View style={styles.container}>
-        <Header />
         <ScrollView style={styles.scrollContainer}>
           <View style={styles.container}>
             <Ranking tipo="classe" status="completo" page="rank"/>           
