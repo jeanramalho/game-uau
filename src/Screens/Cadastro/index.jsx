e45e430d6ba17291ba88ec0832c45c60f419a9cd
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-10 12:09:01 -0300

    Cria screen de cadastro

---
+++ src/Screens/Cadastro/index.jsx
@@ -0,0 +1,25 @@
+import { Text, View, ScrollView } from 'react-native'
+import React, { Component } from 'react'
+import styles from './style'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../../Lib/apollo'
+
+
+
+const Cadastro = () => {
+
+
+
+  return (
+    <ApolloProvider client={client}>
+
+      <View style={styles.container}>    
+     
+      </View>
+
+    </ApolloProvider>
+  )
+
+}
+
+export default Cadastro
