98d72e9f8c5f54a8fbc78a8aae24b85751bce245
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-25 21:59:25 -0300

    declara publicaçào de participante

--- src/Screens/Cadastro/mutation.jsx
+++ src/Screens/Cadastro/mutation.jsx
@@ -2,7 +2,14 @@ import { gql, useMutation } from '@apollo/client'
 
 export const createGamer = gql`
     mutation ($nome: string, $escopo: strging, $pontos: int){
-        createParticipante(data: {}) {
-       
+        createParticipante(data: {nome: $nome, escopo: $escopo, pontos: $pontos}) {
+            id
+            nome
+            escopo
+            pontos
+    }
+    publishParticipante(where: {nome: $nome}, to: PUBLISHED) {
+        id
+      }
     }
 `
