cb10938dc86cebf7b0b12f1c1e7ef65717b0dcbb
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-25 21:55:48 -0300

    declarando variaveis

--- src/Screens/Cadastro/mutation.jsx
+++ src/Screens/Cadastro/mutation.jsx
@@ -1,10 +1,8 @@
 import { gql, useMutation } from '@apollo/client'
 
 export const createGamer = gql`
-    mutation ($nome: string, $escopo: strging, $pontos: int) {
-        int 
-        nome
-        escopo
-        pontos
+    mutation ($nome: string, $escopo: strging, $pontos: int){
+        createParticipante(data: {}) {
+       
     }
 `
