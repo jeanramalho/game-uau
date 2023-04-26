9d6454fb49c6ee75ea410194bd085858f7bba2ff
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-25 22:02:03 -0300

    Termina criação de arquivo mutation

--- src/Screens/Cadastro/index.jsx
+++ src/Screens/Cadastro/index.jsx
@@ -4,6 +4,7 @@ import styles from './style'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../../Lib/apollo'
 import {Picker} from '@react-native-picker/picker';
+import { createGamer } from './mutation'
 
 
 
