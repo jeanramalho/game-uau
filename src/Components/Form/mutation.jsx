487360b4c0fb930318666f3568cf9da7e305fd82
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-02 15:51:27 -0300

    Colocar query em um arquivo separado

--- src/Components/Form/mutation.jsx
+++ src/Components/Form/mutation.jsx
@@ -1,15 +1,15 @@
 import { gql, useMutation } from '@apollo/client'
 
 export const createGamer = gql`
-    mutation ($nome: string, $escopo: strging, $pontos: int){
-        createParticipante(data: {nome: $nome, escopo: $escopo, pontos: $pontos}) {
-            id
-            nome
-            escopo
-            pontos
-    }
-    publishParticipante(where: {nome: $nome}, to: PUBLISHED) {
+mutation ($nome: String!, $pontos: Int!, $escopo: String!){
+    createParticipante(data: {nome: $nome, pontos: $pontos, escopo: $escopo}) {
         id
-      }
-    }
+        nome
+        escopo
+        pontos
+}
+publishParticipante(where: {nome: $nome}, to: PUBLISHED) {
+    id
+  }
+}
 `
