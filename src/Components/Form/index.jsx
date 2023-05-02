970fe047329336d76184376789dad6011a0974de
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-02 15:53:02 -0300

    Começa a ajustar aquivo principal

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -2,24 +2,9 @@ import { Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-nativ
 import React, { Component } from 'react'
 import styles from './style'
 import {Picker} from '@react-native-picker/picker';
-//import { createGamer } from './mutation'
+import { createGamer } from './mutation'
 import { gql, useMutation } from '@apollo/client'
 
-
-
-const createGamer = gql`
-    mutation ($nome: String!, $pontos: Int!, $escopo: String!){
-        createParticipante(data: {nome: $nome, pontos: $pontos, escopo: $escopo}) {
-            id
-            nome
-            escopo
-            pontos
-    }
-    publishParticipante(where: {nome: $nome}, to: PUBLISHED) {
-        id
-      }
-    }
-`
 
 
 
