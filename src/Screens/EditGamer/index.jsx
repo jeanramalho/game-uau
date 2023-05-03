f4b13aee96e900babd0388e2dd18afcbda5a9414
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-03 13:40:24 -0300

    Cria estrutura basica para screen

--- src/Screens/EditGamer/index.jsx
+++ src/Screens/EditGamer/index.jsx
@@ -1,8 +1,14 @@
+import { View } from "react-native"
 import { styles } from "./style"
 
 
 const EditGamer = () => {
 
+        return (
+            <View style={styles.container}>
+
+            </View>
+        )
 }
 
 export default EditGamer
