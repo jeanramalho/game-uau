b9b36313c44d120c32cf44c39a73f0c4d913d2a5
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-03 13:38:49 -0300

    Cria estrutura para screen EditGamer

---
+++ src/Screens/EditGamer/style.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+        container: {
+            width: "100%",
+            padding: 15,
+            marginTop: 40,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+
+})
+
+export default styles
