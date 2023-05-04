0e3c15cf0ce47e4ce22d1b25b290721c53ea4b2b
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-04 13:33:12 -0300

    Importa styles no editcard

--- src/Components/EditCard/index.jsx
+++ src/Components/EditCard/index.jsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
+import styles from './styles'
 
 const EditCard = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Olá Mundo</Text>
         </View>
     )
