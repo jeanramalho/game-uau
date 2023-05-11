d9fe14689fdcb9b7804ec79e370f7b17fd66e9d3
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-11 14:44:26 -0300

    Inverte botões de lugar

--- src/Components/EditCard/index.jsx
+++ src/Components/EditCard/index.jsx
@@ -10,11 +10,14 @@ const EditCard = ({gamer, points, newPoints}) => {
             <Text>+</Text>
             <TextInput style={styles.intupPoint} value={newPoints}/>
 
+            <TouchableOpacity style={styles.buttonStyle}>
+                    <MaterialIcons name="delete" size={18} color="#00cccc"/>
+                </TouchableOpacity>
+
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.buttonStyle}>
                     <Fontisto name="save-1" size={18} color="#00cccc"/>
                 </TouchableOpacity>
-
                 
             </View>
             
