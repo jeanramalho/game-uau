1feac9b7df51023b780b37f1b51231f409c9d21e
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-16 17:34:07 -0300

    Cria coloca conteudo no component editRanking

--- src/Components/EditRanking/index.jsx
+++ src/Components/EditRanking/index.jsx
@@ -5,8 +5,113 @@ import Card from "../Card"
 import { useNavigation } from '@react-navigation/native'
 import { gql, useQuery } from "@apollo/client"
 
-const EditRanking = () => {
-    return ()
+
+const getPart = gql`
+ query Myquery ($escopo: String) {
+  participantes(where: {escopo: $escopo}, orderBy: pontos_DESC, first: 3) {
+    nome
+    pontos
+    escopo
+  }
+}
+`
+
+
+const getPartFull = gql`
+ query Myquery ($escopo: String) {
+  participantes(where: {escopo: $escopo}, orderBy: pontos_DESC) {
+    nome
+    pontos
+    escopo
+  }
+}
+`
+
+
+
+const Ranking = ({tipo, status, page}) => {
+
+  let myQuery = ''
+
+  if (status == "top3") {
+    myQuery = getPart
+  } else if (status == "completo") {
+    myQuery = getPartFull
+  }
+
+    let {loading, error, data, refetch } = useQuery(myQuery, { variables: { escopo: tipo }, fetchPolicy: 'cache-and-network' })
+    if (loading)  {return  <Text>Loading...</Text>}
+    
+   
+   
+
+  const navigation = useNavigation()
+
+  var titleButton = ""
+
+  if (page == 'home') {
+    titleButton = "Ver Ranking Completo"
+  } else {
+    titleButton = "Início"
+  }
+  
+
+  function openRanking(){
+    
+    if(tipo == 'lid' && page == 'home'){
+    
+      navigation.navigate('CompleteRanking')
+      refetch()
+      
+    } else if (tipo == 'classe' && page == 'home')   {
+      navigation.navigate('CompleteRankingClasse')
+      refetch()
+    
+    } else  {
+      navigation.navigate('Home')
+      refetch()
+   
+    }
+  }
+
+  if(tipo == "lid") {
+
+   
+
+    return (
+      
+      <View style={styles.container}>
+      
+        <Text style={styles.title}> Ranking Liderança</Text>
+        {data.participantes.map(participante => {
+          return (
+            <Card key={participante.nome} participante={participante.nome} pontos={participante.pontos} />
+          )
+        })}
+        <TouchableOpacity style={styles.botao} onPress={openRanking}>
+                    <Text style={styles.textBotao}>{titleButton}</Text>
+        </TouchableOpacity>
+      
+      </View>
+    )
+  } else if(tipo == "classe"){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}> Ranking Classe</Text>
+        {data.participantes.map(participante => {
+          return (
+            <Card key={participante.nome} participante={participante.nome} pontos={participante.pontos} />
+          )
+        })}
+        <TouchableOpacity style={styles.botao} onPress={openRanking}>
+                    <Text style={styles.textBotao}>{titleButton}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+ 
+    
+  
 }
 
-export default EditRanking
+export default Ranking
