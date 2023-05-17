17cfdc3e6f9eb54323dbe2b534ed42fadf389596
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-17 18:27:52 -0300

    Ajusta exportação de estilo

--- src/Components/EditRanking/style.jsx
+++ src/Components/EditRanking/style.jsx
@@ -1,7 +1,23 @@
 import { StyleSheet } from "react-native-web";
 
 const styles = StyleSheet.create({
-    container: {
-        
-    }
+    container:{
+        width: "90%",
+        padding: 10,
+        marginTop: 10,
+        backgroundColor: "#cecece",
+        borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    title: {
+        width: '100%',
+        fontSize: 20,
+        marginBottom: 15,
+        textAlign: "center",
+    },
+
 })
+
+export default styles
