5fe99ad9f85c2e0f9244ad9db05134f16c520b6f
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-17 18:34:55 -0300

    Ainda não soluciona mistério de erro

--- src/Components/EditRanking/index.jsx
+++ src/Components/EditRanking/index.jsx
@@ -2,39 +2,39 @@ import {Text, View, Button, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
 import styles from "./style"
 import EditCard from "../EditCard"
-import { gql, useQuery } from "@apollo/client"
+// import { gql, useQuery } from "@apollo/client"
 
 
-const getAllGamers = gql`
- query Myquery {
-  participantes( orderBy: pontos_DESC) {
-    nome
-    pontos
-    escopo
-  }
-}
-`
+// const getAllGamers = gql`
+//  query Myquery {
+//   participantes( orderBy: pontos_DESC) {
+//     nome
+//     pontos
+//     escopo
+//   }
+// }
+// `
 
 
 
 const EditRanking = ({}) => {
 
 
-    let {loading, error, data, refetch } = useQuery(getAllGamers)
-    if (loading)  {return  <Text>Loading...</Text>}
+    // let {loading, error, data, refetch } = useQuery(getAllGamers)
+    // if (loading)  {return  <Text>Loading...</Text>}
   
 
     return (
       
       <View style={styles.container}>
       
-        <Text style={styles.title}> Edit Gamers </Text>
+        {/* <Text style={styles.title}> Edit Gamers </Text>
         {data.participantes.map(participante => {
           return (
             <EditCard key={participante.nome} gamer={participante.nome} points={participante.pontos} />
           )
         })}
-      
+       */}
       </View>
     )
   
