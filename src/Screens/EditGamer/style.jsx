09eb832b712c2aee4c37d7558ff8738238165604
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-18 18:56:03 -0300

    Implementa estilo para página

--- src/Screens/EditGamer/style.jsx
+++ src/Screens/EditGamer/style.jsx
@@ -1,20 +1,47 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    container:{
+        width: "100%",
+        height: "100%",
+        paddingTop: 10,
+        alignItems: "center",
+    },
 
-        container: {
-            width: "100%",
-            padding: 15,
-            marginTop: 40,
-            justifyContent: "center",
-            alignItems: "center",
-        },
+    rankings: {
+        width: "100%",
+        height: "80%",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
 
-        scrollContainer: {
-            width: "100%",
-        },
-    
+    title: {
+        width: "100%",
+        fontSize: 30,
+        fontWeight: "bold",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        marginTop: 20,
+        marginBottom: 20,
+    },
 
+    rankingContainer:{
+        width: "90%",
+        padding: 10,
+        paddingTop:10,
+        marginTop: 10,
+        backgroundColor: "#cecece",
+        borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    scrollContainer: {
+        width: "100%",
+    },
+
+ 
 })
 
 export default styles
