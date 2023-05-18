557a1280609011376ef04c326b4d9adb6954bb4a
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-18 18:55:15 -0300

    Tentando ajustar página do editGamer

--- src/Screens/EditGamer/index.jsx
+++ src/Screens/EditGamer/index.jsx
@@ -1,32 +1,31 @@
 import { Text, View, ScrollView } from 'react-native'
 import React, { Component } from 'react'
 import styles from './style'
+import Card from '../../Components/Card'
 import Ranking from '../../Components/Ranking'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../../Lib/apollo'
 
 
 
-let data = {
-    pontos: 500,
-    novosPontos: 550,
-}
-
 const EditGamer = () => {
 
-        return (
-            <ApolloProvider client={client}>
 
-            <View style={styles.container}>
-              <ScrollView style={styles.scrollContainer}>
-                <View style={styles.container}>
-                  <Ranking />           
-                </View>
-              </ScrollView>
-            </View>
-      
-          </ApolloProvider>   
-        )
+
+  return (
+    <ApolloProvider client={client}>
+
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollContainer}>
+          <View style={styles.container}>
+            <Ranking tipo="classe" status="completo" page="rank"/>           
+          </View>
+        </ScrollView>
+      </View>
+
+    </ApolloProvider>
+  )
+
 }
 
 export default EditGamer
