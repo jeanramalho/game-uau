9d5f2b96a4046105b628690170f7eedf44116ed5
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-18 18:58:35 -0300

    Descomentando codigo para testes

--- src/Components/EditRanking/index.jsx
+++ src/Components/EditRanking/index.jsx
@@ -2,18 +2,18 @@ import {Text, View, Button, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
 import styles from "./style"
 import EditCard from "../EditCard"
-// import { gql, useQuery } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 
 
-// const getAllGamers = gql`
-//  query Myquery {
-//   participantes( orderBy: pontos_DESC) {
-//     nome
-//     pontos
-//     escopo
-//   }
-// }
-// `
+const getAllGamers = gql`
+ query Myquery {
+  participantes( orderBy: pontos_DESC) {
+    nome
+    pontos
+    escopo
+  }
+}
+`
 
 
 
