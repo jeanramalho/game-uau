e5f13caed1ee0f4152a6e59d2dba6d6f1a2d4986
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-25 18:12:47 -0300

    Chamando função no onAction

--- src/Components/EditCard/index.jsx
+++ src/Components/EditCard/index.jsx
@@ -20,7 +20,7 @@ const EditCard = ({gamer, points, newPoints, onAction}) => {
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.buttonStyle}>
-                    <Fontisto name="save-1" size={18} color="#00cccc"  onPress={upGamer}/>
+                    <Fontisto name="save-1" size={18} color="#00cccc"  onPress={onAction}/>
                 </TouchableOpacity>
                 
             </View>
