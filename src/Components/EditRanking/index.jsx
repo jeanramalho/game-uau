dd753607aec0a1719be3ccba6e6728a4f5f618fb
jeanramalho <jeanramalho.dev@gmail.com>
2023-05-26 14:10:30 -0300

    Tentando solução proposta pelo chatgpt

--- src/Components/EditRanking/index.jsx
+++ src/Components/EditRanking/index.jsx
@@ -1,10 +1,85 @@
+// import {Text, View, Button, TouchableOpacity } from 'react-native'
+// import React, { Component } from 'react'
+// import styles from "./style"
+// import EditCard from "../EditCard"
+// import { gql, useQuery, useMutation } from "@apollo/client"
+// import { saveUserChanged } from "./mutation"
+
+
+// const getAllGamers = gql`
+//  query Myquery {
+//   participantes( orderBy: pontos_DESC) {
+//     nome
+//     pontos
+//     escopo
+//   }
+// }
+// `
+
+
+// const EditRanking = ({}) => {
+
+
+//     let {loading, error, data, refetch } = useQuery(getAllGamers)
+   
+//     if (loading)  {return  <Text>Loading...</Text>}
+
+//     // function updateGamer(newPoints, nameGamer) {
+
+//     //   
+
+//     //   gamerChanged({
+//     //     variables: {
+//     //       points: newPoints,
+//     //       gamer: nameGamer,
+//     //     }
+//     //   })
+
+//     //   return alert('Gamer Atualizado com Sucesso!')
+//     // }
+//   const novosPontos = 30547
+
+//     return (
+      
+//       <View style={styles.container}>
+      
+//         <Text style={styles.title}> Edit Gamers </Text>
+//         {data.participantes.map(participante => {
+          
+//           return (
+//             <EditCard key={participante.nome} 
+//             gamer={participante.nome} 
+//             points={participante.pontos} 
+//             onAction={(novosPontos) => {
+//               const [gamerChanged, {}] = useMutation(saveUserChanged)
+
+//               gamerChanged({
+//                 variables: {
+//                   points: novosPontos,
+//                   gamer: participante.nome,
+//                 }
+//               })
+//             }}/>
+//           )
+//         })}
+      
+//       </View>
+//     )
+  
+ 
+    
+  
+// }
+
+// export default EditRanking
+
+
 import {Text, View, Button, TouchableOpacity } from 'react-native'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./style"
 import EditCard from "../EditCard"
 import { gql, useQuery, useMutation } from "@apollo/client"
 import { saveUserChanged } from "./mutation"
-
 
 const getAllGamers = gql`
  query Myquery {
@@ -16,28 +91,25 @@ const getAllGamers = gql`
 }
 `
 
-
 const EditRanking = ({}) => {
 
-
     let {loading, error, data, refetch } = useQuery(getAllGamers)
-   
+    const [novosPontos, setNovosPontos] = useState(0);
+
     if (loading)  {return  <Text>Loading...</Text>}
 
     // function updateGamer(newPoints, nameGamer) {
 
-    //   
+    useEffect(() => {
+      const [gamerChanged, {}] = useMutation(saveUserChanged)
 
-    //   gamerChanged({
-    //     variables: {
-    //       points: newPoints,
-    //       gamer: nameGamer,
-    //     }
-    //   })
-
-    //   return alert('Gamer Atualizado com Sucesso!')
-    // }
-  const novosPontos = 30547
+      gamerChanged({
+        variables: {
+          points: novosPontos,
+          gamer: participante.nome,
+        }
+      })
+    }, [novosPontos]);
 
     return (
       
@@ -51,14 +123,7 @@ const EditRanking = ({}) => {
             gamer={participante.nome} 
             points={participante.pontos} 
             onAction={(novosPontos) => {
-              const [gamerChanged, {}] = useMutation(saveUserChanged)
-
-              gamerChanged({
-                variables: {
-                  points: novosPontos,
-                  gamer: participante.nome,
-                }
-              })
+              setNovosPontos(novosPontos);
             }}/>
           )
         })}
