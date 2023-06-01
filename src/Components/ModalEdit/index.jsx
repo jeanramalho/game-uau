70662c5a56a13e5a8ca7e6db77cd0a886d05b436
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-01 17:49:30 -0300

    edit(ModalEdit): Começa a estruturar componente Modal

--- src/Components/ModalEdit/index.jsx
+++ src/Components/ModalEdit/index.jsx
@@ -2,7 +2,9 @@ import { View, Text, Modal, Button } from "react-native-web"
 import { useState } from "react"
 
 const ModalEdit = () => {
-
+        <View>
+            
+        </View>
 }
 
 export default ModalEdit
